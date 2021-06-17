9a08ab01597641bd537b58a3a73aea97bdc4bc0a
younan <okokoknow@naver.com>
2021-06-17 22:34:43 +0900

    [study] 14장 - custom hook 작성.

--- chapter14-news-viewer/src/components/NewsList.js
+++ chapter14-news-viewer/src/components/NewsList.js
@@ -1,7 +1,8 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import styled from 'styled-components';
 import NewsItem from './NewsItem';
 import axios from 'axios';
+import usePromise from '../lib/usePromise';
 
 const NewsListBlock = styled.div`
     box-sizing: border-box;
@@ -17,37 +18,30 @@ const NewsListBlock = styled.div`
 `;
 
 const NewsList = ({ category }) => {
-    const [articles, setArticles] = useState(null);
-    const [loading, setLoading] = useState(false);
-
-    useEffect(() => {
-        // async를 사용하는 함수 따로 선언
-        const fetchData = async () => {
-            setLoading(true);
-            try {
-                const query = category === 'all' ? '' : `&category=${category}`;
-                const response = await axios.get(
-                    `https://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=a033ee22269a441b81c05a172c442ff0`,
-                );
-                setArticles(response.data.articles);
-            } catch (e) {
-                console.log(e);
-            }
-            setLoading(false);
-        };
-        fetchData();
+    const [loading, response, error] = usePromise(() => {
+        const query = category === 'all' ? '' : `&category=${category}`;
+        return axios.get(
+            `https://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=a033ee22269a441b81c05a172c442ff0`,
+        );
     }, [category]);
 
     // 대기 중일 때
     if (loading) {
         return <NewsListBlock>대기 중...</NewsListBlock>
     }
-    // 아직 articles 값이 설정되지 않았을 때
-    if (!articles) {
+
+    // 아직 response 값이 설정되지 않았을 때
+    if (!response) {
         return null;
     }
 
-    // articles 값이 유효할 때
+    // 에러가 발생했을 때
+    if (error) {
+        return <NewsListBlock>에러 발생!</NewsListBlock>;
+    }
+
+    // response 값이 유효할 때
+    const { articles } = response.data;
     return (
         <NewsListBlock>
             {articles.map(article => (
