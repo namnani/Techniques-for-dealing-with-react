eb01b394ef111c1f6e44d40dd3ea36a8536ff0e1
younan <okokoknow@naver.com>
2021-07-11 23:44:38 +0900

    [study] 5장 - ref: dom 핸들링. - 궁극적으로는, redux등으로 상태관리를 해야하지만, 이런 방법도 있다는 걸 복습..

---
+++ chapter5-ref-dom-naming/src/App.js
@@ -0,0 +1,27 @@
+import ScrollBox from './ScrollBox';
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <ScrollBox ref={(ref) => (this.scrollBox = ref)} />;
+        <button onClick={() => this.scrollBox.scrollToBottom()}>
+          맨 밑으로
+        </button>
+      </div>
+    );
+  }
+}
+
+// const App = () => {
+//   // return <ValidationSample />;
+//   return (
+//     <div>
+//       <ScrollBox ref={(ref) => (this.scrollBox = ref)} />;
+//       <button onClick={() => this.scrollBox.scrollToBottom()}>맨 밑으로</button>
+//     </div>
+//   );
+// };
+
+export default App;
