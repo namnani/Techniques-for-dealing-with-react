adc0cd62716a88ef5f7ccf66ac2cc7c58cb249db
younan <okokoknow@naver.com>
2021-07-21 10:27:37 +0900

    [study] 16장 - vanila redux.  + redux는 react에 종속적인 라이브러리가 아니므로, 바닐라 자바스크립트로 사용해 봄.  + redux의 개념과, reducer의 4 원칙만 다시 복습하면 될 듯.

---
+++ vanila-redux/index.js
@@ -0,0 +1,84 @@
+import { createStore } from "redux";
+
+// DOM 레퍼런스 정의.
+const divToggle = document.querySelector(".toggle");
+const counter = document.querySelector("h1");
+const btnIncrease = document.querySelector("#increase");
+const btnDecrease = document.querySelector("#decrease");
+
+// 액션 타입 정의.
+const TOGGLE_SWITCH = "TOGGLE_SWITCH";
+const INCREASE = "INCREASE";
+const DECREASE = "DECREASE";
+
+// 액션 생성 함수 정의.
+const toggleSwitch = () => ({ type: TOGGLE_SWITCH });
+const increase = (difference) => ({ type: INCREASE, difference });
+const decrease = () => ({ type: DECREASE });
+
+// 초깃값 정의.
+const initialState = {
+  toggle: false,
+  counter: 0,
+};
+
+// 리듀서 함수 정의.
+// state가 undefined일 때는 initialState를 기본값으로 사용
+function reducer(state = initialState, action) {
+  // action.type에 따라 다른 작업을 처리함
+  switch (action.type) {
+    case TOGGLE_SWITCH:
+      return {
+        ...state, // 불변성 유지를 해 주어야 합니다.
+        toggle: !state.toggle,
+      };
+    case INCREASE:
+      return {
+        ...state,
+        counter: state.counter + action.difference,
+      };
+    case DECREASE:
+      return {
+        ...state,
+        counter: state.counter - 1,
+      };
+    default:
+      return state;
+  }
+}
+
+// 스토어 만들기.
+const store = createStore(reducer);
+
+// render 함수 만들기.
+const render = () => {
+  const state = store.getState(); // 현재 상태를 불러옵니다.
+  // 토글 처리
+  if (state.toggle) {
+    divToggle.classList.add("active");
+    console.log(divToggle.classList);
+  } else {
+    divToggle.classList.remove("active");
+    console.log(divToggle.classList);
+  }
+  // 카운터 처리
+  counter.innerText = state.counter;
+};
+
+render();
+
+// 구독하기.
+store.subscribe(render);
+
+//액션 발생시키기.
+divToggle.onclick = () => {
+  store.dispatch(toggleSwitch());
+};
+
+btnIncrease.onclick = () => {
+  store.dispatch(increase(1));
+};
+
+btnDecrease.onclick = () => {
+  store.dispatch(decrease());
+};
