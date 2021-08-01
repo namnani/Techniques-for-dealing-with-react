4aaecb738caddea0147e67a4b49fcbba5f27a897
younan <okokoknow@naver.com>
2021-08-01 23:50:47 +0900

    [study] 18장 - learn redux middleware to support asynchronous task

---
+++ chapter18-learn-redux-middleware/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+// import SampleContainer from './containers/SampleContainer';
+import CounterContainer from './containers/CounterContainer';
+
+function App() {
+  return (
+    <div>
+      <CounterContainer />
+      {/* <SampleContainer /> */}
+    </div>
+  );
+}
+
+export default App;
