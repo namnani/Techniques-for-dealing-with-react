2370b82ea9e43485f087cc00b1d43af3ba5553bc
younan <okokoknow@naver.com>
2021-08-16 23:56:23 +0900

    [react] 14장 뉴스뷰어.

--- chapter14-news-viewer/src/pages/NewsPage.js
+++ chapter14-news-viewer/src/pages/NewsPage.js
@@ -3,15 +3,15 @@ import Categories from '../components/Cattegories';
 import NewsList from '../components/NewsList';
 
 const NewsPage = ({ match }) => {
-    // 카테고리가 선택되지 않았으면 기본값 all로 사용
-    const category = match.params.category || 'all';
+  // 카테고리가 선택되지 않았으면 기본값 all로 사용
+  const category = match.params.category || 'all';
 
-    return (
-        <>
-            <Categories />
-            <NewsList category={category} />
-        </>
-    );
+  return (
+    <>
+      <Categories />
+      <NewsList category={category} />
+    </>
+  );
 };
 
 export default NewsPage;
